ab6311624037b87bf51b2e618a9b9e7396dba5ed
Daniel Heery <danieljheery@gmail.com>
2022-09-29 15:26:48 +0000

    section 1 - 6

---
+++ 5-Dynamic-Content-And-Templating-Engines/nodejs-practice-site/routes/shop.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const path = require('path');
+const rootDir = require('../util/path')
+const router = express.Router();
+const adminData = require('./admin');
+
+router.get('/', (req, res, next) => {
+  const products = adminData.products;
+  res.render('shop', { products: products, docTitle: 'Shop' });
+}) 
+
+module.exports = router;
